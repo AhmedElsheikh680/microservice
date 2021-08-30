638da99ba23554bc03b7f9f29368e4f9776cf056
Ahmed Elsheikh <ahmedelsheikh680@gmail.com>
2021-08-30 16:19:17 +0200

    helloWorldBeanhelloWorldBeanWithParam

--- src/main/java/com/rest/webservice/controller/HelloWorldCOntroller.java
+++ src/main/java/com/rest/webservice/controller/HelloWorldCOntroller.java
@@ -2,6 +2,8 @@ package com.rest.webservice.controller;
 
 import com.rest.webservice.model.HelloWorldBean;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,5 +17,15 @@ public class HelloWorldCOntroller {
     @GetMapping("/api/v1/helloWorldBean")
     public HelloWorldBean helloWorldBean(){
         return  new HelloWorldBean("Hello World Bean");
+    }
+
+    @GetMapping("/api/v1/helloWorldBean/{name}")
+    public HelloWorldBean helloWorldBeanWithPathVariable(@PathVariable String name){
+        return new HelloWorldBean("Hello Wolrd Bean " + name);
+    }
+
+    @GetMapping("/api/v1/helloWorldBeanWithParam")
+    public HelloWorldBean helloWorldBeanWithParam(@RequestParam String name){
+        return new HelloWorldBean("Hello Wolrd Bean " + name);
     }
 }
